ed621470a84c187959d57ff5c1c013d0b9e24b4e
Hansberry <chransberry@gmail.com>
2018-11-13 13:24:29 -0700

    Fixed on error and got another...

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,7 +7,7 @@ class Sidebar extends React.Component {
 				<input type="text" placeholder="Filter restaurants" onChange={e => this.props.filterResults(e.target.value)} />
 				<ul>
 					{this.props.places.map((place, i) => {return (
-						<li key={place.id} className="info" onClick={this.props.triggerMarker(this.props.allMarkers[i])}>
+						<li key={place.id} className="info" onClick={() => {this.props.triggerMarker(this.props.allMarkers[i])}}>
 							<h3>{place.name}</h3>
 							<p>{place.cuisine}</p>
 							<a href={place.website} target="_blank">Visit website</a>
